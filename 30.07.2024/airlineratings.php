b26c6b50fbd76ebd788f449801c5f4e1f36df9e3
Ahmed Salem <ahmedsalem_1991@yahoo.com>
2024-07-30 22:34:27 +0300

    dasd

---
+++ 30.07.2024/airlineratings.php
@@ -0,0 +1,101 @@
+<?php
+
+class thegulfheraldcom extends plugin_base
+{
+
+	// ANT settings
+	protected $ant_precision = 6;
+	protected $agent = 'Mozilla/5.0 (compatible; Yahoo! Slurp; http://help.yahoo.com/help/us/ysearch/slurp)';
+	protected $use_proxies = true;
+
+
+	// CRAWL settings
+	protected $stop_on_article_found = false;
+	protected $stop_date_override = true;
+	protected $stop_on_date = false;
+
+	// DEFINITIONS
+	protected $site_timezone = 'Asia/Amman';
+	protected $logic = array(
+		'list1' => array(
+			0 => array(
+				'type' => 'list2',
+				'regexp' => '/^(.*)$/Uis',
+				'append_domain' => false,
+				'process_link' => 'process_list1_link',
+			)
+		),
+		'list2' => array(
+			0 => array(
+				'type' => 'article',
+				'regexp' => '/<loc>(.*)<\/loc>/Uis',
+				'append_domain' => false,
+				'process_link' => 'process_article_link'
+			)
+		),
+		'article' => array(
+			'headline' => '/<h1[^<]*>(.*)<\/h1>/Uis',
+			'content' => '/<div class=td-post-content>(.*)<footer>/Uis',
+			'author' => false,
+			'article_date' => '/datePublished":"(.*)"/Uis'
+		)
+	);
+
+	protected function process_list1_link($link, $referer_link, $logic)
+	{
+
+		$temp_link = ''; // https://www.airlineratings.com/post-sitemap7.xml
+		if (preg_match_all('/<loc>(https:\/\/www\.airlineratings\.com\/post-sitemap\d+?\.xml)<\/loc>/Uis', $link, $matches)) {
+			$temp_link = $matches[0][sizeof($matches[0]) - 1];
+			$temp_link = str_replace('<loc>', '', $temp_link);
+			$temp_link = str_replace('</loc>', '', $temp_link);
+		}
+
+		return $temp_link;
+	}
+
+	private $links = array();
+	private $array_index = 0;
+
+	protected function process_article_link($link, $referer_link, $logic)
+	{
+
+		$temp_link = '';
+		if (empty($this->links)) {
+			$result = $this->ant->get($referer_link);
+			if (preg_match_all('/<loc>(.*)<\/loc>/Uis', $result, $matches)) {
+				$this->links = $matches[0];
+				$this->array_index = sizeof($this->links);
+			}
+		}
+		$this->array_index--;
+		if ($this->array_index > 0 and isset($this->links[$this->array_index])) {
+			$temp_link = str_replace('<loc>', '', $this->links[$this->array_index]);
+			$temp_link = str_replace('</loc>', '', $temp_link);
+			return $temp_link;
+		}
+
+		return '';
+	}
+
+	
+
+	// process the date of the article, return in YYYY-MM-DD HH:ii:ss format
+	protected function process_date($article_date) {
+
+		//2024-07-30T06:29:14+00:00 
+		if (preg_match('/(.*)T(.*)\+/Uis', $article_date, $matches)) {
+
+			$article_date_obj = DateTime::createFromFormat(
+				'Y-m-d H:i:s',
+				$matches[1] . ' ' . $matches[2],
+				new DateTimeZone($this->site_timezone)
+			);
+			$article_date = $article_date_obj->format('Y-m-d H:i:s');
+		}
+        
+		return $article_date;
+
+	}
+
+}
