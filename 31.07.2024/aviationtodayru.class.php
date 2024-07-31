487e2c23c40d1b9c4a55f62d48609311b7054558
Ahmed Salem <ahmedsalem_1991@yahoo.com>
2024-07-31 11:22:19 +0300

    asd

---
+++ 31.07.2024/aviationtodayru.class.php
@@ -0,0 +1,61 @@
+<?php
+
+class aviationtodayru extends plugin_base {
+
+	// ANT settings
+	protected $ant_precision = 6;
+	protected $stop_on_date = false;
+	protected $agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/64.0.3282.167 Chrome/64.0.3282.167 Safari/537.36';
+
+	// CRAWL settings
+	protected $stop_on_article_found = true;
+	protected $stop_date_override = true;
+
+	// DEFINITIONS
+	protected $site_timezone = 'Asia/Amman';
+
+
+	protected $logic = array(
+		'list1' => array(
+			0 => array(
+				'type' => 'list1',
+				'regexp' => '/<div class="pagination">.*class="next" href="(.*)"/Uis',
+				'append_domain' => false
+			),
+			1 => array(
+				'type' => 'article',
+				'regexp' => [
+					'/<div class="category_news">(.*)<div class="pagination">/Uis',
+					'/<div class="title"><a href="(.*)"/Uis',
+				],
+				'append_domain' => false
+			)
+		),
+		'article' => array(
+			'headline' => '/<h1[^<]*>(.*)<\/h1>/Uis',
+			'content' => '/<div class="fpm_start"><\/div>(.*)<div class="fpm_end"><\/div>/Uis',
+			'author' => false,
+			'article_date' => '/dateModified":"(.*)"/Uis'
+		)
+	);
+
+
+
+	// process the date of the article, return in YYYY-MM-DD HH:ii:ss format
+	protected function process_date($article_date) {
+
+		//2018-05-21T08:20:26+00:00
+		if (preg_match('/(.*)T(.*)\+/Uis', $article_date, $matches)) {
+
+			$article_date_obj = DateTime::createFromFormat(
+				'Y-m-d H:i:s',
+				$matches[1] . ' ' . $matches[2],
+				new DateTimeZone($this->site_timezone)
+			);
+			$article_date = $article_date_obj->format('Y-m-d H:i:s');
+		}
+
+		return $article_date;
+
+	}
+}
